39238a38bce462a67638b17b568fb344246a4fd7
KALEKRSTOS731 <kalekirstoszeru@gmail.com>
2025-01-14 22:26:45 +0300

    quiz.js

--- quiz.js
+++ quiz.js
@@ -1,8 +1,16 @@
 
 function checkAnswer() {
     var correctAnswer="4";
+
 }
 document.querySelector('input[name="quiz"]:checked');
 userAnswer = selectedOption.value;
+const feedbackElement = document.getElementById("feedback");
+if (userAnswer === correctAnswer) {
+    feedbackElement.textContent = "Correct! Well done.";
+  } else {
+    feedbackElement.textContent = "Incorrect. Try again!";
+  }
+
 
 
