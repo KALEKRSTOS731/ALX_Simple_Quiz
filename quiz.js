d6557c7d07b8a1667b7a4982c5ae239886e6cfc6
KALEKRSTOS731 <kalekirstoszeru@gmail.com>
2025-01-14 22:47:35 +0300

    quiz.js

--- quiz.js
+++ quiz.js
@@ -10,7 +10,13 @@ if (userAnswer === correctAnswer) {
     feedbackElement.textContent = "Correct! Well done.";
   } else {
     feedbackElement.textContent = "Incorrect. That's incorrect. Try again!";
+
   }
+  submitButton.addEventListener("click", function () {
+    console.log("Submit Answer button clicked!");
+    // Add your logic here, such as checking the user's answer
+  });
+  submitButton.addEventListener("click", checkAnswer);
 
 
 
